ccbeef08f45cbda99729ca995f163d1d7e61c423
Paulo Ascenção <frozsyn@gmail.com>
2017-06-06 18:08:20 +0100

    Update setup.sh

--- Installation/setup.sh
+++ Installation/setup.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-if [["$EUID" -ne 0 ]]; then
+if [[ "$EUID" -ne 0 ]]; then
+  
   echo "Run this script as root."
   exit
+
 fi
 
 yum -y install epel-release
-yum -y install expect wget httpd php php-mysql php-pdo php-gd php-mbstring php-imap php-ldap mariadb-server mariadb
+yum -y install expect wget httpd php php-mysqç php-pdo php-gd php-mbstring php-imap -phpldap mariadb-server mariadb
 
 firewall-cmd --permanent --add-service=http
 firewall-cmd --reload
@@ -21,14 +23,14 @@ MYSQL_SECURE_INSTALLATION=$(expect -c "
 set timeout 10
 spawn mysql_secure_installation
 expect \"Enter current password for root (enter for none):\"
-send \"\r\"
+send \"n\r\"
 expect \"Change the password?\"
 send \"n\r\"
 expect \"Remove anonymous users?\"
 send \"y\r\"
 expect \"Disallow root login remotely?\"
-send \"y\r\"
-expect "Remove test database and access to ir?\"
+sebd \"y\r\"
+expect \"Remove test database and access to it?\"
 send \"y\r\"
 expect \"Reload privilege tables now?\"
 send \"y\r\"
@@ -36,10 +38,10 @@ expect eof
 ")
 echo "$MYSQL_SECURE_INSTALLATION"
 
-mysql -u root << 'EOF' 
+mysql -u root << 'EOF'
 CREATE DATABASE glpi;
-CREATE USER 'glpi@localhost' IDENTIFIED BY 'Pa$$w0rd';
-GRANT ALL PRIVILEGES ON glpi.* TO 'glpi'@'localhost' IDENTIFIED BY 'Pa$$w0rd';
+CREATE USER 'glpi'@'localhost' IDENTIFIED BY 'Pa$$w0rd';
+GRANT ALL PRIVILIGES ON glpi.* TO 'glpi@localhost' IDENTIFIED BY 'Pa$$w0rd';
 FLUSH PRIVILEGES;
 EOF
 
@@ -47,10 +49,10 @@ wget https://github.com/glpi-project/glpi/releases/download/9.1.2/glpi-9.1.2.tgz
 tar -xvf glpi*.tgz -C /var/www/html/
 rm -rf glpi*.tgz
 
-chmod -R 755 /var/www/html/
-chmod -R apache:apache /var/www/html/glpi
+chmod -R 755 /var/www/html/glpi
+chown -rf glpi*.tgz
 
-chcon -R -t httpd_sys_rw_content_t /var/www/html/glpi
+chcon -R -t httpd_sys_rw_content_t /var/ww/html/glpi
 
 setsebool -P httpd_can_network_connect 1
 setsebool -P httpd_can_network_connect_db 1
