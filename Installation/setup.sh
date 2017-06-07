1ad4f3243106a3a80fb743340c4eb90c81169eda
Paulo Ascenção <frozsyn@gmail.com>
2017-06-07 14:09:50 +0100

    Update setup.sh

--- Installation/setup.sh
+++ Installation/setup.sh
@@ -8,7 +8,7 @@ if [[ "$EUID" -ne 0 ]]; then
 fi
 
 yum -y install epel-release
-yum -y install expect wget httpd php php-mysqç php-pdo php-gd php-mbstring php-imap -phpldap mariadb-server mariadb
+yum -y install expect wget httpd php php-mysql php-pdo php-gd php-mbstring php-imap php-ldap mariadb-server mariadb
 
 firewall-cmd --permanent --add-service=http
 firewall-cmd --reload
@@ -23,13 +23,13 @@ MYSQL_SECURE_INSTALLATION=$(expect -c "
 set timeout 10
 spawn mysql_secure_installation
 expect \"Enter current password for root (enter for none):\"
-send \"n\r\"
+send \"\r\"
 expect \"Change the password?\"
 send \"n\r\"
 expect \"Remove anonymous users?\"
 send \"y\r\"
 expect \"Disallow root login remotely?\"
-sebd \"y\r\"
+send \"y\r\"
 expect \"Remove test database and access to it?\"
 send \"y\r\"
 expect \"Reload privilege tables now?\"
@@ -41,7 +41,7 @@ echo "$MYSQL_SECURE_INSTALLATION"
 mysql -u root << 'EOF'
 CREATE DATABASE glpi;
 CREATE USER 'glpi'@'localhost' IDENTIFIED BY 'Pa$$w0rd';
-GRANT ALL PRIVILIGES ON glpi.* TO 'glpi@localhost' IDENTIFIED BY 'Pa$$w0rd';
+GRANT ALL PRIVILEGES ON glpi.* TO 'glpi'@'localhost' IDENTIFIED BY 'Pa$$w0rd';
 FLUSH PRIVILEGES;
 EOF
 
@@ -50,9 +50,9 @@ tar -xvf glpi*.tgz -C /var/www/html/
 rm -rf glpi*.tgz
 
 chmod -R 755 /var/www/html/glpi
-chown -rf glpi*.tgz
+chown -R apache:apache /var/www/html/glpi
 
-chcon -R -t httpd_sys_rw_content_t /var/ww/html/glpi
+chcon -R -t httpd_sys_rw_content_t /var/www/html/glpi
 
 setsebool -P httpd_can_network_connect 1
 setsebool -P httpd_can_network_connect_db 1
