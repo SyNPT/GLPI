204fd6a9a54e1a4efcb777e0a05ad6623cffc9b8
Paulo Ascenção <frozsyn@gmail.com>
2017-06-09 19:44:48 +0100

    Create Install_plugin_modifications.sh

---
+++ Configuration/Install_plugin_modifications.sh
@@ -0,0 +1,12 @@
+cd
+
+wget https://sourceforge.net/projects/glpithemes/files/PLUGIN/Plugin_mod-9.1.2-1.1.0.tar.gz
+
+gunzip Plugin_mod-9.1.2-1.1.0.tar.gz
+
+tar -xvf Plugin_mod-9.1.2-1.1.0.tar
+
+mv ~/mod /var/www/html/glpi/plugins/
+
+
+
