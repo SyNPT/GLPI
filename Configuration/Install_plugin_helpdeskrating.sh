9f9ee541c03f8631660798c4848c3bca54ffc2d9
Paulo Ascenção <frozsyn@gmail.com>
2017-06-10 20:04:13 +0100

    Create Install_plugin_helpdeskrating.sh

---
+++ Configuration/Install_plugin_helpdeskrating.sh
@@ -0,0 +1,9 @@
+cd
+
+yum -y install unzip
+
+wget https://sourceforge.net/projects/helpdeskrating/files/Helpdeskrating%201.1.1/helpdeskrating_1.1.1.zip
+
+unzip helpdeskrating_1.1.1.zip
+
+mv ~/helpdeskrating_1.1.1.zip /var/www/html/glpi
